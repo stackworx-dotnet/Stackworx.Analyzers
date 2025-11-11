ef6745d4d6b1fddcc4f9ef4c231fba2bd62461fd
Ciaran Liedeman <ciaran@stackworx.io>
2025-11-11 09:27:46 +0200

    Analyze generated source

--- Stackworx.Analyzers/GraphQLUnusedDataLoaderAnalyzer.cs
+++ Stackworx.Analyzers/GraphQLUnusedDataLoaderAnalyzer.cs
@@ -27,7 +27,8 @@ public sealed class GraphQLUnusedDataLoaderAnalyzer : DiagnosticAnalyzer
     public override void Initialize(AnalysisContext context)
     {
         context.EnableConcurrentExecution();
-        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.ConfigureGeneratedCodeAnalysis(
+            GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
         context.RegisterCompilationStartAction(StartDataLoaderAnalysis);
     }
 
